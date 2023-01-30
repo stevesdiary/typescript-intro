f85848804988a4c96c30e52b523e453fda289f59
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2023-01-30 15:48:04 +0100

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+const app: Application = express();
+const add = (a: number, b: number): number => a + b;
+
+app.get('/', (req: Request, res: Response, next: NextFunction) =>{
+   console.log(add(5, 6))
+   res.send('Hello World!');
+});
+
+app.listen(500, () => console.log('Server running'));
