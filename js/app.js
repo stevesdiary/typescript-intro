a3bfb8e1a371654f974e1bf5042c2466b21015ad
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2023-02-02 01:04:08 +0100

    Typescript functions

--- js/app.js
+++ js/app.js
@@ -1,62 +1,70 @@
 "use strict";
-// Objects, Arrays, Tuples, & Enums
-Object.defineProperty(exports, "__esModule", { value: true });
-let stringArr = ['one', 'hey', 'chicken'];
-let guitars = ['strat', 'Les Paul', 5150];
-let mixedData = ['EVH', 1984, true];
-stringArr[0];
-stringArr.push('red');
-guitars[0] = 1984;
-guitars.unshift('Jim');
-guitars = stringArr;
-mixedData = guitars;
-//guitars = mixedData  //the type of data has restricted us from reassigning anothe typeof array to it but it can work the other way round.
-// the same goes for guitars and stringArr
-let test = [];
-let bands = [];
-bands.push('Fanny James');
-//Tuple  (Tuple can be used to strictly specify the position of the elements of an array)
-let myTuple = ['Dave', 45, true];
-// for example
-let mixed = ['Jonah', 5, false];
-myTuple[1] = 23; //the position of the el;ement allowable to be added can only be the type of the element in that position.
-//Objects\
-let myObj;
-myObj = [];
-console.log(typeof myObj);
-myObj = bands;
-myObj = {};
-const exampleObj = {
-    prop1: 'Salome',
-    prop2: true
+//type Aliases:  Here the type of values is represented by an alias which can be used to denote the type whenever we want to use it.
+// Object.defineProperty(exports, "__esModule", { value: true });
+//interface postId = stringOrNumber  // Interface doesn't work with type Aliases.
+// Literal types
+let userName;
+userName = 'Amy'; //Using any other username other than (Dave, John or Amy) will flag error because they have been declared literally so it puts it in a limit of only what is declared.
+//Functions: the type of input of function can also be specified including that of the expected return value
+const add = (a, b) => {
+    return a + b;
 };
-//Similar to the tuples, prop1 and prop2 can only be assigned string and boolean respectively
-exampleObj.prop1 = 'John';
-let evh = {
-    name: 'Eddie',
-    active: false,
-    albums: [1983, 2021, 'OU1218']
+const logMsg = (message) => {
+    console.log(message);
 };
-let jp = {
-    name: 'Jimmy',
-    active: true,
-    albums: ['I', 'II', 'IV']
+logMsg('Hello!');
+logMsg(add(2, 3));
+// logMsg(add('a',3))
+let subtract = function (c, d) {
+    return c - d;
 };
-const greetGuitarist = (guitarist) => {
-    if (guitarist.name) {
-        return `Hello ${guitarist.name.toUpperCase()}!`;
+// interface mathFunction {
+//    (a: number, b: number):number
+// }
+let multiply = function (c, d) {
+    return c * d;
+};
+logMsg(multiply(2, 2));
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
     }
-    return 'Hello!';
+    return a + b;
 };
-console.log(greetGuitarist(jp));
-//Enums
-// 'Unlike most TypeScript features, Enums are not a type-level Addition to javascript but something added to the language and runtime.'
-var Grade;
-(function (Grade) {
-    Grade[Grade["U"] = 0] = "U";
-    Grade[Grade["D"] = 1] = "D";
-    Grade[Grade["C"] = 2] = "C";
-    Grade[Grade["B"] = 3] = "B";
-    Grade[Grade["A"] = 4] = "A";
-})(Grade || (Grade = {}));
-console.log(Grade.U);
+// default param value
+const sumAll = (a = 10, b, c = 2) => {
+    return a + b + c;
+};
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(2, 3));
+logMsg(sumAll(undefined, 3)); //Note that default value will not work with type Alias like we did with mathFunction 
+// Rest Parameter
+const total = (...nums) => {
+    return nums.reduce((prev, curr) => prev + curr);
+};
+logMsg(total(10, 2, 3));
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
+const infinite = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        if (i > 100)
+            break;
+    }
+};
+//Custom type guard
+const isNumber = (value) => {
+    return typeof value === 'number'
+        ? true : false;
+};
+// Use of never type
+const numberOrString = (value) => {
+    if (typeof value === 'string')
+        return 'string';
+    if (isNumber(value))
+        return 'number';
+    return createError('This should never happen!');
+};
--- src/app.ts
+++ src/app.ts
@@ -1,93 +1,97 @@
-// Objects, Arrays, Tuples, & Enums
+//type Aliases:  Here the type of values is represented by an alias which can be used to denote the type whenever we want to use it.
 
 import { type } from "os"
 
-let stringArr = ['one', 'hey', 'chicken']
+type stringOrNumber = string | number
 
-let guitars = ['strat', 'Les Paul', 5150]
-
-let mixedData = ['EVH', 1984, true]
-
-stringArr[0]
-stringArr.push('red')
-guitars[0] = 1984
-
-guitars.unshift('Jim')
-
-guitars = stringArr
-
-mixedData = guitars
-//guitars = mixedData  //the type of data has restricted us from reassigning anothe typeof array to it but it can work the other way round.
-                     // the same goes for guitars and stringArr
-
-let test = []
-let bands: string[] = []
-bands.push('Fanny James')
-
-//Tuple  (Tuple can be used to strictly specify the position of the elements of an array)
-let myTuple: [string, number, boolean] = ['Dave', 45, true]
-// for example
-let mixed = ['Jonah', 5, false]
-
-myTuple[1] = 23  //the position of the el;ement allowable to be added can only be the type of the element in that position.
-
-//Objects\
-let myObj: object
-myObj = []
-console.log(typeof myObj)
-
-myObj =  bands
-myObj = {}
-
-const exampleObj = {
-   prop1: 'Salome',
-   prop2: true
-}
-//Similar to the tuples, prop1 and prop2 can only be assigned string and boolean respectively
-exampleObj.prop1 = 'John'
-
-type Guitarist = {
-   name: string,
-   active?: boolean,             // The ? make it an optional value (boolean and undefined)
-   albums: (string | number)[]
-}
-interface Guitarists {
+type stringOrNumberArray = (string | number)[]
+type Guitarists = {
    name?: string,
-   active: boolean,             //Objects can also be defined by using interface
-   // When we have optional ? on the name or active but while trying to use the attributes of string or boolean, the ?(optional) symbol must be put on the attributes whenever it is called.
-   albums: (string | number)[]
+   active: boolean,
+   albums: stringOrNumberArray
+}
+type UserId = stringOrNumber
+
+//interface postId = stringOrNumber  // Interface doesn't work with type Aliases.
+
+
+// Literal types
+
+let userName: 'Dave' | 'John' | 'Amy'
+userName = 'Amy'  //Using any other username other than (Dave, John or Amy) will flag error because they have been declared literally so it puts it in a limit of only what is declared.
+
+
+//Functions: the type of input of function can also be specified including that of the expected return value
+const add = (a: number, b: number): number => {
+   return  a + b
 }
 
-
-let evh: Guitarist = {
-   name: 'Eddie',
-   active: false,
-   albums: [1983, 2021, 'OU1218']
+const logMsg = (message: any): void => {
+   console.log(message)
 }
 
-let jp: Guitarist = {
-   name: 'Jimmy',
-   active: true,
-   albums:['I', 'II', 'IV']
+logMsg('Hello!')
+logMsg(add(2,3))
+// logMsg(add('a',3))
+
+let subtract = function (c: number, d: number):
+number {
+   return c - d
 }
 
-const greetGuitarist = (guitarist: Guitarist) => {
-   if(guitarist.name){
-      return `Hello ${guitarist.name.toUpperCase()}!`
+type mathFunction = (a: number, b: number) => number
+// interface mathFunction {
+//    (a: number, b: number):number
+// }
+let multiply: mathFunction = function (c, d) {
+   return c * d 
+}
+logMsg(multiply(2,2))
+
+const addAll = (a: number, b:number, c?: number): number => {
+   if( typeof c !== 'undefined') {
+      return a + b + c
    }
-   return 'Hello!'
+   return a + b
+}
+// default param value
+const sumAll = (a: number=10, b: number, c: number = 2): number => {
+   return a + b + c
+} 
+logMsg(addAll(2, 3, 2))
+logMsg(addAll(2, 3))
+logMsg(sumAll(2, 3 ))
+logMsg(sumAll(undefined, 3))  //Note that default value will not work with type Alias like we did with mathFunction 
+
+// Rest Parameter
+
+const total = (...nums: number[]): number =>{
+   return nums.reduce((prev, curr)=> prev + curr)
 }
 
-console.log(greetGuitarist(jp))
+logMsg(total(10, 2, 3))
 
-//Enums
-// 'Unlike most TypeScript features, Enums are not a type-level Addition to javascript but something added to the language and runtime.'
 
-enum Grade {
-   U,  // If U = 1, then the default U= 0 will be overwritten and all other elements of enum(grade) will follow as well.
-   D,
-   C,
-   B,
-   A
+const createError = (errMsg: string): never => {
+   throw new Error(errMsg)
 }
-console.log(Grade.U)
+
+const infinite = () => {
+   let i: number = 1
+   while (true) {
+      i++
+      if ( i > 100) break
+   }
+} 
+//Custom type guard
+const isNumber = (value: any): boolean => {
+   return typeof value === 'number'
+   ? true : false
+}
+
+// Use of never type
+const numberOrString = (value: number | string): string => {
+   if (typeof value === 'string') return 'string'
+   if (isNumber(value)) return 'number'
+   return createError('This should never happen!')
+}
